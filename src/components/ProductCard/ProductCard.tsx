e67f7140d454ac7b8e1f7a5fc81126cf74486d1e
Ali Helmi <ahelmi365@gmail.com>
2024-10-25 17:03:25 +0300

    use data in ProductCard

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,32 +1,41 @@
-interface IProps {}
+import { IProduct } from "../../interfaces/inedx";
+import { sliceText } from "../../utils/functions";
 import Button from "../ui/Button/Button";
 import Image from "./../Image/Image";
 
-const ProductCard = ({}: IProps) => {
-  return (
-    <div className="border p-2 rounded-md flex flex-col">
-      <Image
-        url="https://d2tez01fe91909.cloudfront.net/wp-content/uploads/2024/02/shutterstock_743270791.jpg.webp"
-        alt="product name"
-        className="scale-95 hover:scale-100 transition-all ease-in-out duration-500 rounded-md mb-2"
-      />
+interface IProps {
+  product: IProduct;
+}
 
-      <h3>2022 Mozila Car</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, quos sit
-        aspernatur tempore
-      </p>
+
+const ProductCard = ({ product }: IProps) => {
+  const {  title, describtion, imageURL, price, colors, category } = product;
+  return (
+    <div className="order p-2 border rounded-md flex flex-col ">
+      <div className={""} >
+        <Image
+          url={imageURL}
+          alt={title}
+          className="scale-95 hover:scale-100 transition-all ease-in-out duration-500 rounded-md mb-2"
+        />
+      </div>
+
+      <h3>{title}</h3>
+      <p>{sliceText(describtion, 60)}</p>
       <div className="flex gap-1 my-2">
-        <span className="w-5 h-5 bg-indigo-500 rounded-full cursor-pointer"></span>
-        <span className="w-5 h-5 bg-green-500 rounded-full cursor-pointer"></span>
-        <span className="w-5 h-5 bg-red-500 rounded-full cursor-pointer"></span>
+        {colors.map((color) => (
+          <span
+          key={color}
+            className={`w-5 h-5 bg-${color} rounded-full cursor-pointer border-2`}
+          ></span>
+        ))}
       </div>
 
       <div className="flex justify-between items-center">
-        <span>$500,000</span>
+        <span>${price}</span>
         <Image
-          url="https://d2tez01fe91909.cloudfront.net/wp-content/uploads/2024/02/shutterstock_743270791.jpg.webp"
-          alt="product name"
+          url={category.imageURL}
+          alt={category.name}
           className="h-10 w-10 border rounded-full object-center cursor-pointer"
         />
       </div>
