1eccbd44edba2226180a1fb21498b7bf06f14ff4
Ali Helmi <ahelmi365@gmail.com>
2024-10-25 17:03:55 +0300

    use data in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import ProductCard from "./components/ProductCard/ProductCard";
+import { products } from "./data";
 
 function App() {
+  const renderedProductList = products.map((product) => (
+    <ProductCard product={product} key={product.id} />
+  ));
   return (
-    <div className="container m-auto p-2">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+    <main className="container m-auto p-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-2">
+        {renderedProductList}
       </div>
-    </div>
+    </main>
   );
 }
 
