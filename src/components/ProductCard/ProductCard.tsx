f5ab9f9dd00d939623384c70859c382574e448f2
Ali Helmi <ahelmi365@gmail.com>
2024-10-26 16:04:57 +0300

    clear ProdctCard comp

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -5,14 +5,14 @@ import Image from "./../Image/Image";
 
 interface IProps {
   product: IProduct;
+  setProduct: React.Dispatch<React.SetStateAction<IProduct>>;
 }
 
-
-const ProductCard = ({ product }: IProps) => {
-  const {  title, describtion, imageURL, price, colors, category } = product;
+const ProductCard = ({ product, setProduct }: IProps) => {
+  const { id, title, describtion, imageURL, price, colors, category } = product;
   return (
     <div className="order p-2 border rounded-md flex flex-col ">
-      <div className={""} >
+      <div className={""}>
         <Image
           url={imageURL}
           alt={title}
@@ -25,7 +25,7 @@ const ProductCard = ({ product }: IProps) => {
       <div className="flex gap-1 my-2">
         {colors.map((color) => (
           <span
-          key={color}
+            key={color}
             className={`w-5 h-5 bg-${color} rounded-full cursor-pointer border-2`}
           ></span>
         ))}
@@ -33,21 +33,25 @@ const ProductCard = ({ product }: IProps) => {
 
       <div className="flex justify-between items-center">
         <span>${price}</span>
-        <Image
+        {/* <Image
           url={category.imageURL}
           alt={category.name}
           className="h-10 w-10 border rounded-full object-center cursor-pointer"
-        />
+        /> */}
       </div>
-      <div className="flex gap-8 mt-4">
-        <Button
-          className="bg-green-500 border-green-300  text-white"
-          onClick={() => alert("clicked")}
-        >
-          Success
-        </Button>
+      <div className="flex justify-between gap-8 mt-4">
+     
         <Button className="bg-blue-500 border-blue-300 text-white">Edit</Button>
-        <Button className="bg-red-500 rder-red-300   text-white">Delete</Button>
+        <Button
+          className="bg-red-500 rder-red-300 text-white"
+          onClick={() => {
+            setProduct(product)
+            const dialog = document.querySelector("dialog");
+            dialog?.showModal();
+          }}
+        >
+          Delete
+        </Button>
       </div>
     </div>
   );
