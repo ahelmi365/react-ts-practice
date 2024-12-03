39d9f20fb60fc632f86be0ca00b4778e81e3b7cc
Ali Helmi <ahelmi365@gmail.com>
2024-12-03 10:49:44 +0200

    remove classes

--- src/components/ui/InputField/InputField.tsx
+++ src/components/ui/InputField/InputField.tsx
@@ -12,7 +12,7 @@ const InputField = ({ ...rest }: IProps) => {
   return (
     <input
       {...rest}
-      className="border-2 outline-none p-1.5 rounded-md w-full mb-3 shadow-md focus:border-indigo-400 focus:ring-2 focus:ring-indigo-300"
+      className="border-2 outline-none p-1.5 rounded-md w-full shadow-md focus:border-indigo-400 focus:ring-2 focus:ring-indigo-300"
     />
   );
 };
